1b7f2fb4c039d2bf7f33f36bc800a3eda3728ad6
Andreas Sørensen <70892717+OneDrizzle@users.noreply.github.com>
2020-09-14 11:37:28 +0200

    En Lommeregner med Classes
    Eksempel p[ brug af clsses og Methods i en Lommeregner
    
    Co-Authored-By: NickDahlHansen <71008723+NickDahlHansen@users.noreply.github.com>

---
+++ Lommeregn Classes/Lommeregn/Calculater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lommeregn
+{
+    public class Calculater
+    {
+        public int Add(int tal1, int tal2)
+        {
+            return tal1 + tal2;
+        }
+        public int Subtract(int tal1, int tal2)
+        {
+            return tal1 - tal2;
+        }
+        public double Devide(double tal1, double tal2)
+        {
+            return tal1 / tal2;
+        }
+        public int Multiply(int tal1, int tal2)
+        {
+            return tal1 * tal2;
+        }
+    }
+}
---
+++ Lommeregn Classes/Lommeregn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lommeregn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculater Calc = new Calculater();
+            Console.WriteLine(Calc.Add(1, 2));
+            Console.WriteLine(Calc.Subtract(5, 7));
+            Console.WriteLine(Calc.Devide(8, 2));
+            Console.WriteLine(Calc.Multiply(10, 3));
+            Console.ReadLine();
+        }
+        
+    }
+}
