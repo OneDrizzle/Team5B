8b627e516bc227d698f4418aa18508d7c5d6d027
OneDrizzle <70577990+Akks26490@users.noreply.github.com>
2020-09-23 14:33:51 +0200

    Ændret nogle Classes

---
+++ HYDAC/HYDAC/MeetingRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HYDAC
+{
+    class MeetingRoom
+    {
+        private string _name;
+
+        public MeetingRoom(string _name)
+        {
+           
+        }
+    }
+}
--- HYDAC/HYDAC/Pamphlet.cs
+++ HYDAC/HYDAC/Pamphlet.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace HYDAC
 {
-    class MRBP
+    class Pamphlet
     {
     }
 }
