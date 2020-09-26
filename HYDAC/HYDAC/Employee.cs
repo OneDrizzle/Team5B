2e9e092bd2f6d9242b85384a86516afc6ce2d931
C1PH3R5 <30761466+C1PH3R5@users.noreply.github.com>
2020-09-26 10:40:01 +0200

    employee: V3,1

--- HYDAC/HYDAC/Employee.cs
+++ HYDAC/HYDAC/Employee.cs
@@ -20,16 +20,10 @@
         }
         public void AddGuest(Guest guest)
         {
-            _guests[0] = guest;
-
-
             // hent data fra controller i form af et array 
-
-
             // insert data into guest array 
-
-
             // find ud af hvordan i indsætter mood i array, i den rigtige rakke føgle 
+            _guests[_guests.Length + 1] = guest;
 
 
             // finde ud af hvordan vi tjekker antal i huset (matematric " array.lenght / 4!" )
@@ -38,8 +32,8 @@
 
             // En måde og tjekke hvor mange pladser brugt. Length. Index´etc
 
-            //string date = DateTime.Now.ToString("dddd, dd MMMM");
-            // dato, navn, firma, ansvarlig, ankomst, afgang, sikkerhedsfolder
+
+
 
 
         }
