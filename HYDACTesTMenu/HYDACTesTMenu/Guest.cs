eaf84767ab05b67c337e1ed049793eeb655fc1fa
BrianVith <71009099+BrianVith@users.noreply.github.com>
2020-09-26 20:23:48 +0200

    Testmenu RemoveGuest() idé
    Har lavet et bud på hvordan vi kan løse problemet med RemoveGuest()

---
+++ HYDACTesTMenu/HYDACTesTMenu/Guest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HYDACTesTMenu
+{
+    public class Guest
+    {
+        private string _name;
+        private string _phone;
+        private string _mail;
+        private bool _present;
+        public Guest(string name, string phone, string mail)
+        {
+            _name = name;
+            _phone = phone;
+            _mail = mail;
+        }
+
+        public string Name // lavede en property for at teste koden
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public void SetPresence(bool present)
+        {
+            _present = present;
+        }
+    }
+}
--- HYDACTesTMenu/HYDACTesTMenu/Program.cs
+++ HYDACTesTMenu/HYDACTesTMenu/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HYDACTesTMenu;
+using System;
 
 namespace HydacMenu
 {
@@ -6,7 +7,70 @@ namespace HydacMenu
     {
         static void Main(string[] args)
         {
+            
+            Guest[] _guests = new Guest[10];
+            int counter = 0; // holder styr på hvor mange gæster der er oprettet
 
+            _guests[0] = new Guest("John", "00000", "mail");
+            counter++;
+            _guests[1] = new Guest("Hans", "11111", "mail");
+            counter++;
+            _guests[2] = new Guest("Peter", "22222", "mail");
+            counter++; // counter er nu 3
+            
+            string name = "Hans"; //navn på gæst der skal fjernes fra _guests[] array
+            
+            Console.WriteLine("gamle gæste-array:");
+
+            for (int i = 0; i < counter; i++)
+            {
+                Console.WriteLine(_guests[i].Name + "" + i); // udskriver gæster før vi laver ændringer + deres plads i arrayet
+
+                // da der jo altid er op til 10 pladser var jeg nødt til at bruge 'counter' hele vejen ellers->
+                // kom der null exception hvis jeg prøvede at udskrive fx [3] eller [4]...[9] 
+            }
+
+            Guest[] temp = new Guest[counter]; //opretter et temp[] Guest array
+
+            int j = 0;
+            int tempcounter = counter; //midlertidig counter der ved hvor mange gæster der var oprettet i _guests[]
+            for (int i = 0; i < tempcounter; i++)
+            {
+                if (_guests[i].Name != name) 
+                {
+                    temp[i-j] = _guests[i];  //tilføjer kun gæst i temp[] array hvis navnet ikke er "Hans"
+                }
+                else
+                {
+                    j++;   // 'j' sørger for at vi sætter elementer ind på den korrekte plads i vores temp[] array --->
+                           // uden 'j' ville vi sætte [0]=[0] og [2]=[2] ... da 1=1 springes overeftersom gæsten hedder "Hans"->
+                           // og derfor skal vi sætte [0]=[0] og [1]=[2] så "Peter" får den rigtige plads i temp[] array
+
+                    counter--;  // holder styr på det antallet af elementer i temp[] array
+                }
+            }
+
+            Console.WriteLine("\nNye gæste-array:");
+
+            for (int i = 0; i < counter; i++)
+            {
+                _guests[i] = temp[i]; // her sætter vi _guests[] tilbage til det temp[] array vi lavede
+                Console.WriteLine(_guests[i].Name + "" + i); // udskriver det ændrede _guest[] array + gæstens plads i arrayet
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+            /*
             Console.Clear();
             Console.Write("\n\t\t\t\t\t\t\tHydac Menu\n\n\n\t\t\t\t\t1. Login \t2. Admin login \t\t3. exit\n\nt\t\t\t\t\t\t\tChose menu: ");
             int parseMenu;
@@ -30,7 +94,7 @@ namespace HydacMenu
                 {
                     /*Console.Clear();
                     Console.WriteLine("This is menu:{0}\n\t\t\t\t\t\t\tHydac Menu\n\n\n\t\t\t\t\tThis is menu:{0}", parseMenu);
-                    Console.ReadLine();*/
+                    Console.ReadLine();
                     
                 }
                 else
@@ -51,7 +115,7 @@ namespace HydacMenu
             //Her kan det blive svært når der er to der laver noget i samme fil
             //nu kan det blive lidt svært
             //It works? :o
-
+            */
         }
     }
 }
