b2c72d068b74553cc0a793f48b95583845d84f0b
BrianVith <brianvithdk@gmail.com>
2020-09-29 11:14:59 +0200

    added Menu + Menuitem

---
+++ HYDAC/HYDAC/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HYDAC
+{
+    public class MenuItem
+    {
+        private string title;
+
+        public MenuItem(string itemTitle)
+        {
+            title = itemTitle;
+        }
+
+        public string Title
+        {
+            get { return title;}
+        }
+    }
+}
