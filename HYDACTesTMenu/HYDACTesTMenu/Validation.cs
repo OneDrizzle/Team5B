f5ab42977ab8cdf6ca3183f00e4d276ae7378c9b
C1PH3R5 <30761466+C1PH3R5@users.noreply.github.com>
2020-10-01 16:06:09 +0200

    Validation: v1.0

---
+++ HYDACTesTMenu/HYDACTesTMenu/Validation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Valdation
+{
+    public class Valdation
+    {
+        private bool _returnBool;
+        private int _returnValue;
+        private int _MenuLength;
+        string _returnMessage;
+        public bool MenuInputValdation(string userInput, int MenuLength)
+        {
+            _MenuLength = MenuLength;
+
+            if (int.TryParse(userInput, out int parseInput))
+            {
+                for (int i = 0; i < _MenuLength; i++)
+                {
+                    if (i > 1 || i < _MenuLength)
+                    {
+                        _returnMessage = "Forkert input! 1 - " + MenuLength + " Prøv igen.";
+                        _returnBool = false;
+                    }
+                    else
+                    {
+                        _returnValue = parseInput;
+                        _returnBool = true;
+                    }
+                }
+
+            }
+            else
+            {
+                _returnMessage = "Forkert input! Prøv igen.";
+                _returnBool = false;
+            }
+            return _returnBool;
+
+        }
+        public int ReturnValue()
+        {
+            return _returnValue;
+        }
+        public string ReturnMessage()
+        {
+            return _returnMessage;
+        }
+
+    }
+}
+
+
+/*
+ * InputValdationClass validate = new InputValdationClass();
+ * if (validate.MenuInputValdation(Console.ReadLine(), 6))
+ *      {      
+ *          switch (validate.ReturnValue())
+ *          {
+ *              case 1:
+ *                  Console.WriteLine("Menu 1")
+ *                  Break;
+ *              defualt:
+ *                  Break;
+ *          }
+ *      }
+ *      else
+ *      {
+ *      Console.WriteLine(validate.ReturnValue());
+ *      }
+*/
