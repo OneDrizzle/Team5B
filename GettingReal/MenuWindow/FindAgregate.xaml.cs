b9aebaf8198efb119e555e9c9d31ad0572441dae
OneDrizzle <akks26490@edu.ucl.dk>
2020-11-13 10:32:07 +0100

    NU virker validering!

--- GettingReal/MenuWindow/FindAgregate.xaml.cs
+++ GettingReal/MenuWindow/FindAgregate.xaml.cs
@@ -1,5 +1,4 @@
-﻿using GettingReal;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,17 +23,47 @@ namespace MenuWindow
 
 
 
+        int count = 0;
+        string filename = "";
         private void GetOrderNumber_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (FindOrderNumber.Text.Length != 6)
             {
                 btn_OpenAggregateFile.IsEnabled = false;
+                lbl_Error.Content = ("");
             }
 
             else if (FindOrderNumber.Text.Length == 6)
             {
                 btn_OpenAggregateFile.IsEnabled = true;
 
+                string SelectedOrderNumber = FindOrderNumber.Text;
+
+                string targetPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string path = @"GettingReal\GettingReal\Aggregates\";
+
+                int indexOfPath = targetPath.IndexOf("GettingReal");
+                if (indexOfPath >= 0)
+                    targetPath = targetPath.Remove(indexOfPath);
+                string destFile = System.IO.Path.Combine(targetPath, path);
+
+                DirectoryInfo dir = new DirectoryInfo(destFile);
+                FileInfo[] files = dir.GetFiles(SelectedOrderNumber + "*", SearchOption.TopDirectoryOnly);
+
+                foreach (FileInfo fileFound in files)
+                {
+                    filename = fileFound.Name;
+                    count++;
+                }
+                if (count > 0)
+                {
+                    btn_OpenAggregateFile.IsEnabled = true;
+                }
+                else
+                {
+                    btn_OpenAggregateFile.IsEnabled = false;
+                    lbl_Error.Content = ("Dette ordrenummer eksisterer ikke");
+                }
 
             }
 
@@ -44,7 +73,7 @@ namespace MenuWindow
         {
             string SelectedOrderNumber = FindOrderNumber.Text;
             //VentilationAggregateRepository aggregateRepo = new VentilationAggregateRepository();
-            
+
             //VentilationAggregate aggregate = aggregateRepo.GetVentilationAggregate(SelectedOrderNumber);
 
             string targetPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
@@ -57,12 +86,9 @@ namespace MenuWindow
 
             DirectoryInfo dir = new DirectoryInfo(destFile);
             FileInfo[] files = dir.GetFiles(SelectedOrderNumber + "*", SearchOption.TopDirectoryOnly);
-            foreach (FileInfo fileFound in files)
-            {
-                string NewPath = Path.Combine(fileFound.Name, destFile + System.IO.Path.GetFileName(fileFound.Name));
 
-                Process.Start(new ProcessStartInfo(NewPath) { UseShellExecute = true });
-            }
+            string NewPath = Path.Combine(filename, destFile + System.IO.Path.GetFileName(filename));
+            Process.Start(new ProcessStartInfo(NewPath) { UseShellExecute = true });
 
             ProjectChefWindow main = new ProjectChefWindow();
             main.Show();
