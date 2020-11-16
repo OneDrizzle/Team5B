d1f94ee306bbc76d25be4a08d7ac22f3dec2fcdd
BrianVith <71009099+BrianVith@users.noreply.github.com>
2020-11-16 14:43:42 +0100

    Save/Load minors v2.02

--- GettingReal/MenuWindow/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ GettingReal/MenuWindow/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace MenuWindow {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MenuWindow;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MenuWindow;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
